5fb063863c89d34137a7eff745d86fca61ce04d2
Ivan <ivan-student@outlook.com>
2021-01-27 17:51:32 +0700

    Add index routing

---
+++ lesson-5/scr/otusapp.blank/Controllers/IndexController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace otusapp.blank.Controllers
+{
+    /// <summary>
+    /// Health check
+    /// </summary>
+    [ApiController]
+    [AllowAnonymous]
+    [Route("")]
+    [Produces("application/json")]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new
+            {
+                version = "2"
+            });
+        }
+    }
+}

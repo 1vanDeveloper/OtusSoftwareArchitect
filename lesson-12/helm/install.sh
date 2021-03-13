1274699400675d570e1ab0205e3f831c317a1916
Ivan <ivan-student@outlook.com>
2021-03-13 09:09:05 -0800

    Add RBAC access kube-params for gateway service discovery

--- lesson-12/helm/install.sh
+++ lesson-12/helm/install.sh
@@ -1,3 +1,8 @@
 sudo helm install identity ./identity
 sudo helm install account ./account
 sudo helm install gateway ./gateway
+sudo kubectl create clusterrolebinding permissive-binding \
+  --clusterrole=cluster-admin \
+  --user=admin \
+  --user=kubelet \
+  --group=system:serviceaccounts

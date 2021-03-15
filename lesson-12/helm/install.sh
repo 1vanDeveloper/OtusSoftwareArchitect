e4d47ec800954eaa4a160dc2f4ec3553eee4e5ca
Ivan <ivan-student@outlook.com>
2021-03-15 09:39:43 -0700

    Update scripts

--- lesson-12/helm/install.sh
+++ lesson-12/helm/install.sh
@@ -1,6 +1,12 @@
+# need for gateway configuration
+sudo kubectl config set-context --current --namespace=otus-services
+
+# install services
 sudo helm install identity ./identity
 sudo helm install account ./account
 sudo helm install gateway ./gateway
+
+# add for gateway service discovery https://ocelot.readthedocs.io/en/latest/features/kubernetes.html
 sudo kubectl create clusterrolebinding permissive-binding \
   --clusterrole=cluster-admin \
   --user=admin \
--- lesson-12/prometheus/install.sh
+++ lesson-12/prometheus/install.sh
@@ -1,8 +1,8 @@
 # disable standerd ingress controller
 sudo minikube addons disable ingress
-# setting monitoring namespace
-sudo kubectl create namespace monitoring
-sudo kubectl config set-context --current --namespace=monitoring
+# setting otus-services namespace
+sudo kubectl create namespace otus-services
+sudo kubectl config set-context --current --namespace=otus-services
 # add prometheus and grapfana repository for installing
 sudo helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 sudo helm repo add stable https://charts.helm.sh/stable
@@ -13,5 +13,6 @@ sudo helm install prom prometheus-community/kube-prometheus-stack -f prometheus.
 sudo helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 sudo helm repo update
 # install ingress-controller and postgres exporter like helm application
-sudo helm install nginx ingress-nginx/ingress-nginx -f nginx-ingress.yaml #--atomic
-sudo kubectl get all
+sudo helm install nginx ingress-nginx/ingress-nginx -f nginx-ingress.yaml --skip-crds #--atomic
+
+sleep 30s

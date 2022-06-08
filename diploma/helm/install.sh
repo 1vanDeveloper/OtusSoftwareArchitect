46f0fe45fdaa7299c1871fcfdcdd38749828248e
ivan <ivan-student@outlook.com>
2022-06-08 23:49:26 +0300

    Start migrate from ocelot to nginx gateway

--- diploma/helm/install.sh
+++ diploma/helm/install.sh
@@ -4,6 +4,13 @@ kubectl config set-context --current --namespace=otus-services
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
+# add for gateway service discovery https://ocelot.readthedocs.io/en/latest/features/kubernetes.html
+kubectl create clusterrolebinding permissive-binding \
+  --clusterrole=cluster-admin \
+  --user=admin \
+  --user=kubelet \
+  --group=system:serviceaccounts
+
 # install services
 helm install rabbitmq-service bitnami/rabbitmq -f ./rabbit/values.yaml
 helm install redis-service bitnami/redis -f ./redis/values.yaml
@@ -11,12 +18,6 @@ helm install identity ./identity
 helm install billing ./billing
 helm install account ./account
 helm install order ./order
-helm install gateway ./gateway
 helm install notification ./notification
-
-# add for gateway service discovery https://ocelot.readthedocs.io/en/latest/features/kubernetes.html
-kubectl create clusterrolebinding permissive-binding \
-  --clusterrole=cluster-admin \
-  --user=admin \
-  --user=kubelet \
-  --group=system:serviceaccounts
+# helm install gateway ./gateway
+helm install gateway bitnami/nginx -f ./nginx/values.yaml -f ./nginx/custom-server-block.yaml
